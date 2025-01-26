82bf16d01f466f1faef659e8c7df1ec83f882868
Alekao18 <umzehonorio@gmail.com>
2025-01-26 14:59:13 -0300

    Importando o modelo de linguagem

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Arquivo principal do projeto
 
-import langchain
+import os
+from langchain_groq import ChatGroq
+from langchain.prompts import ChatPromptTemplate
 import torch
 import transformers
+
+CHAT_GROQ_JARVIS_API = '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'
+os.environ ['API_KEY'] = CHAT_GROQ_JARVIS_API
+chat = ChatGroq(model='llama-3.3-70b-versatile')
+
